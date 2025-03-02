281e099962a2d63a547e6d1272964562bb125eb1
peterobumneme <peterobumneme334@gmail.com>
2025-03-02 01:07:09 +0100

    stati

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash - && \
+apt-get install -y nodejs && \
+npm install -g npm@latest
+
+# Install Python dependencies using pip
+pip install -r requirements.txt
+
+# Collect static files and build Tailwind CSS
+python manage.py collectstatic --noinput
+python manage.py tailwind build
